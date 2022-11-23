06c6788101205115654f2c653eeb9b1bac9d7901
Nishimwe Prince <princeelysee@gmail.com>
2022-11-23 20:43:01 +0200

    Adding modal

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
 // Variables
 
-document.getElementById("form-container").addEventListener('submit', getBookmarks);
+document.getElementById("form-container").addEventListener('submit', saveBookmark);
+document.getElementById('modalbtn').addEventListener('click', showModal);
+
+let modalWrap = null;
+
+function showModal(){
+
+modalWrap = document.createElement('div');
+
+modalWrap.innerHTML = `
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+`;
+
+document.body.append(modalWrap);
+
+let modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
+modal.show();
+}
+
 
 
 
 function saveBookmark(e){
+    e.preventDefault();
 
     let sitename = document.getElementById("sitename").value;
     let siteurl = document.getElementById("siteurl").value;
     let category = document.getElementById("category").value;
+
+    if (category == "Please choose a category"){
+        window.prompt("Please select a category!");
+    }
 
     let bookmark = {
         name: sitename,
@@ -18,9 +61,9 @@ function saveBookmark(e){
 
     console.log(bookmark);
 
-    if (localStorage.getItem('bookmarks') == null) {
+    let bookmarks = [];
 
-        let bookmarks = [];
+    if (localStorage.getItem('bookmarks') == null) {
 
         bookmarks.push(bookmark);
     
@@ -36,7 +79,8 @@ function saveBookmark(e){
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
 
-    console.log(category);
+    location.reload()
+    console.log(bookmark);
 
 }
 
@@ -53,26 +97,26 @@ function getBookmarks(){
     console.log(bookmarks);
 
     
-    for (let i=0; i<bookmarks.length; i++){
+    for (let i=0; i<bookmarks?.length; i++){
         let name = bookmarks[i].name;
         let url = bookmarks[i].url;
         let category = bookmarks[i].category;
 
         let li = document.createElement('li');
         li.className = "list-group-item";
-        li.innerHTML = name;
+        li.innerHTML += name;
 
         if (category == "Social Media"){
-            bookmarksSocials.append(name);
+            bookmarksSocials.append(li);
         }
         else if(category == "Education"){
-            bookmarksEducation.append(name);
+            bookmarksEducation.append(li);
         }
         else if (category == "Entertainment"){
-            bookmarksEntertainment.append(name);
+            bookmarksEntertainment.append(li);
         }
         else if (category == "Other"){
-            bookmarksOther.append(name);
+            bookmarksOther.append(li);
         }
         else{
             break;
