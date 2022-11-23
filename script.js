3b180c3fce89715d8ded34c4af62e7d7c356be92
Nishimwe Prince <princeelysee@gmail.com>
2022-11-23 02:37:59 +0200

    Fixing final bugs

--- script.js
+++ script.js
@@ -36,11 +36,48 @@ function saveBookmark(e){
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
 
+    console.log(category);
+
 }
 
-function getBookmarks(e){
-    e.preventDefault();
-    let bookmarks = localStorage.getItem('bookmarks');
+function getBookmarks(){
+    let bookmarks = [];
+    
+    bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+    let bookmarksSocials = document.getElementById("socialmedia-list");
+    let bookmarksEducation = document.getElementById("education-list");
+    let bookmarksEntertainment = document.getElementById("entertainment-list");
+    let bookmarksOther = document.getElementById("other-list");
 
     console.log(bookmarks);
+
+    
+    for (let i=0; i<bookmarks.length; i++){
+        let name = bookmarks[i].name;
+        let url = bookmarks[i].url;
+        let category = bookmarks[i].category;
+
+        let li = document.createElement('li');
+        li.className = "list-group-item";
+        li.innerHTML = name;
+
+        if (category == "Social Media"){
+            bookmarksSocials.append(name);
+        }
+        else if(category == "Education"){
+            bookmarksEducation.append(name);
+        }
+        else if (category == "Entertainment"){
+            bookmarksEntertainment.append(name);
+        }
+        else if (category == "Other"){
+            bookmarksOther.append(name);
+        }
+        else{
+            break;
+        }
+    }
+
+
 }
